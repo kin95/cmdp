ab659a45df5651116e63bdc1fc8dd3f20c8f3a73
kin95 <kin95@126.com>
2021-12-27 14:58:23 +0800

    update start.sh

--- start.sh
+++ start.sh
@@ -10,6 +10,6 @@ fi
 bogon:test dy_bom$ cat start.sh 
 #!/bin/sh
 rm -f tpid
-nohup java -jar ./corporate-mobile-donation-registration-0.0.1-8443.jar --spring.profiles.active=prod &
+nohup java -jar ./corporate-mobile-donation-registration-0.0.1-8443.jar > /home/ec2-user/cmdp-app/log/cmdp.log  2>&1 &
 echo $! > tpid
 echo Start Success!
